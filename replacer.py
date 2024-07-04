bc506c476c9468545867c5ff42f1d3bd4b77d8f2
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2024-07-04 15:45:35 +0200

    update index to 0.5.29
    -fix all remaining utf8-encoding issues
    -improve replacer.py

--- replacer.py
+++ replacer.py
@@ -1,6 +1,6 @@
 path = "C:/Users/Leander/Documents/Python/verschlüsseln_gesamt/büro_webseite/index.html"
 with open(path, "r", encoding="utf-8") as f:
     content_org = f.read()
-content = content_org.replace("ß", "&szlig;",).replace("ü", "&uuml;").replace("ö", "&ouml;")
+content = content_org.replace("ß", "&szlig;",).replace("ü", "&uuml;").replace("ö", "&ouml;").replace("Ü", "&Uuml;").replace("Ö", "&Ouml;").replace("ä", "&auml;").replace("Ä", "&Auml;").replace("b&uuml;", "bü")
 with open(path, "w", encoding="utf-8") as f:
     f.write(content)
