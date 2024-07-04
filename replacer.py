8f8ab1d4e5f6631fe807509c8a2feac070fb86ee
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2024-07-04 16:43:05 +0200

    update index to 0.6.37
    -update all project files
    -add information
    -fix structure bug

--- replacer.py
+++ replacer.py
@@ -1,4 +1,4 @@
-path = "C:/Users/Leander/Documents/Python/verschlüsseln_gesamt/büro_website2/index.html"
+path = "C:/Users/Leander/Documents/Python/verschlüsseln_gesamt/büro_webseite/index.html"
 with open(path, "r", encoding="utf-8") as f:
     content_org = f.read()
 content = content_org.replace("ß", "&szlig;",).replace("ü", "&uuml;").replace("ö", "&ouml;").replace("Ü", "&Uuml;").replace("Ö", "&Ouml;").replace("ä", "&auml;").replace("Ä", "&Auml;").replace("b&uuml;", "bü")
