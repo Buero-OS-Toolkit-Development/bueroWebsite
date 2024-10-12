6c9987d41d381c546c20736519b20ac212c5d84c
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2024-10-12 11:23:58 +0200

    update * to 1.2.51
    -update replacer to show replaced target count
    -replace 21 targets

--- replacer.py
+++ replacer.py
@@ -1,6 +1,9 @@
+from naturalsize import listToInt
 path = "C:/Users/Leander/Documents/Python/verschlüsseln_gesamt/büro_webseite/index.html"
 with open(path, "r", encoding="utf-8") as f:
     content_org = f.read()
+invalid = ["ß", "ü", "ö", "Ö", "ü", "ä", "Ä", "b&uuml;", " />"]
+print("Targets found: "+str(listToInt([content_org.count(i) for i in invalid])))
 content = content_org.replace("ß", "&szlig;",).replace("ü", "&uuml;").replace("ö", "&ouml;").replace("Ü", "&Uuml;").replace("Ö", "&Ouml;").replace("ä", "&auml;").replace("Ä", "&Auml;").replace("b&uuml;", "bü").replace(" />", "/>")
 with open(path, "w", encoding="utf-8") as f:
     f.write(content)
