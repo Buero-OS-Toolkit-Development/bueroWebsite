0f0c1c3dff050fdb035d1be65fe2a49bf97d96b5
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2024-10-12 12:46:22 +0200

    update * to 1.2.52
    -add version info and other infos to replacer
    -replace 8 targets

--- replacer.py
+++ replacer.py
@@ -1,4 +1,7 @@
 from naturalsize import listToInt
+
+print("Booting replacer v1.2.52")
+print("replacer is part of Leander Kafemanns project bueroWebsite.")
 path = "C:/Users/Leander/Documents/Python/verschlüsseln_gesamt/büro_webseite/index.html"
 with open(path, "r", encoding="utf-8") as f:
     content_org = f.read()
@@ -7,3 +10,4 @@ print("Targets found: "+str(listToInt([content_org.count(i) for i in invalid])))
 content = content_org.replace("ß", "&szlig;",).replace("ü", "&uuml;").replace("ö", "&ouml;").replace("Ü", "&Uuml;").replace("Ö", "&Ouml;").replace("ä", "&auml;").replace("Ä", "&Auml;").replace("b&uuml;", "bü").replace(" />", "/>")
 with open(path, "w", encoding="utf-8") as f:
     f.write(content)
+print("Targets successfully replaced.")
