c1d862929b3e2055dbee9f1fca1b90da7dd9b24d
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2024-10-15 20:25:09 +0200

    Delete js/username.js
    rename file

--- js/username.js
+++
@@ -1,8 +0,0 @@
-function usernameCheck() {
-    if (getCookie("username", document.cookie) == "") {
-        setCookie("username", prompt("Nutzernamen eingeben:", "Testuser"), 7);
-    }
-    if (getCookie("code", document.cookie) == "") {
-        setCookie("code", prompt("Bestaetigungscode eingeben:", "111111"), 7)
-    }
-}
