96a9629a8329ebaeb2f7e9a85dc1960118793af4
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2024-10-15 17:42:00 +0200

    update set_like

--- js/set_like.js
+++ js/set_like.js
@@ -2,7 +2,7 @@
     Script for setting likes.
 */
 async function setLike(contentHeader) {
-    document.getElementById(contentHeader).textContent = "Likes werden geladen..."
+    document.getElementById(contentHeader).textContent = "Likes werden geladen...";
     let name_ = getCookie("username", document.cookie);
     let code = getCookie("code", document.cookie);
     await fetch("https://lkunited.pythonanywhere.com/webResources/alterLikes?username=" + name_ + "&code=" + code + "&contentHeader=" + contentHeader);
