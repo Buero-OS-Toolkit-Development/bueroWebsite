fb77b686460ebddd1db6ada183f07a2d644efcef
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2025-02-21 17:59:48 +0100

    add v3.3 files

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+﻿function renameMenuPoints() {
+    let menuTitlesShort = ["🏠", "🛈", "🡇", "🔧", "📦", "🖼", "💻", "🔗", "☎"];
+    let menuTitlesLong = ["Home", "Über", "Installation", "Features", "Pakete", "Bilder", "Entwickler", "Weblinks", "Kontakt"]
+    if (getCookie("rearranged", document.cookie) == "true") {
+        setCookie("rearranged", "false", 7);
+        for (let i = 0; i < menuTitlesLong.length; i++) {
+            document.getElementById("menu" + (i+1).toString()).textContent = menuTitlesLong[i];
+        }
+        document.getElementById("buttonSwitchMP").textContent = "Symbole";
+    } else {
+        setCookie("rearranged", "true", "7");
+        for (let i = 0; i < menuTitlesShort.length; i++) {
+            document.getElementById("menu" + (i+1).toString()).textContent = menuTitlesShort[i];
+        }
+        document.getElementById("buttonSwitchMP").textContent = "Texte";
+    }
+}
